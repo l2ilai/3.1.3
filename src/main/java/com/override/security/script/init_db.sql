6faf180a2e2470c28dcd3d97392a512697da3d9c
l2ilai <yoasakura13@gmail.com>
2024-07-15 00:02:31 +0400

    added getting the user's name from the сaptio

--- src/main/java/com/override/security/script/init_db.sql
+++ src/main/java/com/override/security/script/init_db.sql
@@ -2,4 +2,5 @@ INSERT INTO public.t_role(id, role)
 VALUES (1, 'ROLE_USER'), (2, 'ROLE_ADMIN');
 INSERT INTO public.t_user(id, name, last_name, email, age, password)
 VALUES (1, 'admin', 'admin', 'admin@admin', 10, '$2y$10$G42kF6YujsyKOtjlZKT2Re8F1fGR40CJVhsr3lakHEmieAHKL9Hq.');
-
+INSERT INTO public.t_user_roles(user_id, roles_id)
+VALUES (1, 2);
